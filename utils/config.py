b50226f44d6cf108f6a6d1355244e25f7fa08909
v3gas90b <v3gas90b@gmail.com>
2026-03-05 16:36:28 +0100

    created page object for home page and a couple of basic functions, and a couple of tests to test the functionality

--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,4 @@
 class Config:
-    AUTH_URL = "https://grocerymate.masterschool.com/auth"
+    HOME_PAGE_URL = "https://grocerymate.masterschool.com/"
+    AUTH_URL = HOME_PAGE_URL + "auth"
     DEFAULT_TIMEOUT = 10
