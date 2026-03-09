f45ccfa04bd9f3285c1cf3c2311d53b6a3ee4d16
v3gas90b <v3gas90b@gmail.com>
2026-03-09 21:18:21 +0100

    updated config.py with the checkout page url, added and updated checkout_page.py with functions of the page, added and updated test_checkout_page.py to test basic functionality

--- utils/config.py
+++ utils/config.py
@@ -2,6 +2,7 @@ class Config:
     HOME_PAGE_URL = "https://grocerymate.masterschool.com/"
     AUTH_URL = HOME_PAGE_URL + "auth"
     SHOP_PAGE_URL = HOME_PAGE_URL + "store"
+    CHECKOUT_PAGE_URL = HOME_PAGE_URL + "checkout"
     DEFAULT_TIMEOUT = 10
     ALL_CATEGORY = 1  # All, by default
     ITEM_QUANTITY = 5
